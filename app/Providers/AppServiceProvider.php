d8459642dbffc93dee395d47c7c2d52799c1eacc
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-07-23 10:40:36 +0900

    add:application service追加

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,14 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Applications\Services\ArticleService;
+use App\Applications\Services\IArticleService;
+use App\Repositories\Article\ArticleRepository;
+use App\Repositories\Article\IArticleRepository;
+use App\Repositories\ArticleComment\ArticleCommentRepository;
+use App\Repositories\ArticleComment\IArticleCommentRepository;
+use App\Repositories\Media\MediaRepository;
+use App\Repositories\Media\IMediaRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(IArticleService::class, ArticleService::class);
+        $this->app->bind(IArticleRepository::class, ArticleRepository::class);
+        $this->app->bind(IArticleCommentRepository::class, ArticleCommentRepository::class);
+        $this->app->bind(IMediaRepository::class, MediaRepository::class);
     }
 
     /**
