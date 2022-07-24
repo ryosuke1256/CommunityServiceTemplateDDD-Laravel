d4de741301b6692b403a87d40eb94f4439572e84
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-07-24 12:18:39 +0900

    refactor:不要な記述を削除

--- app/Repositories/Article/IArticleRepository.php
+++ app/Repositories/Article/IArticleRepository.php
@@ -5,13 +5,10 @@ namespace App\Repositories\Article;
 
 use App\Domains\User\User;
 use App\Domains\Article\Article;
-use App\Domains\ArticleStatus\ArticleStatus;
-use App\Domains\ArticleCategory\ArticleCategory;
 
 interface IArticleRepository
 {
     public static function getAll(): array;
-    // public static function getArticles(int $displayCount): array;
     public static function save(User $user, Article $article): void;
     public static function edit(User $user, Article $article): void;
     public static function delete(int $articleId): void;
